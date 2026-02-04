64fedc70a6c703f5639e2642f3e787d68f7e305b
Pascal <pascalkollmer@gmail.com>
2026-02-04 18:13:36 +0100

    change data_repo_tester fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,7 +10,9 @@ pytest_plugins = ["pooch_doi.testkit"]
 @pytest.fixture(scope="session")
 def data_repo_tester(create_data_repo_tester_type):
     return create_data_repo_tester_type(
-        DataverseRepository, base_url_fallback="https://dataverse.org"
+        DataverseRepository, 
+        archive_base_url_fallback="https://dataverse.org",
+        api_base_url_fallback="https://dataverse.org",
     )
 
 """
